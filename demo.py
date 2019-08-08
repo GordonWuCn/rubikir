88c53e3e33a9955f0fab3e973513f8298ea80549
whoiscc <correctizer@gmail.com>
2019-08-08 22:28:51 +0800

    Update IR
    Signed-off-by: whoiscc <correctizer@gmail.com>

--- demo.py
+++ demo.py
@@ -11,11 +11,21 @@ ip_program = Program(queue_api=True)
 # name declarations
 # variable is per-packet temporary storage
 identifier = ip_program.variable('identifier')
-get_all = ip_program.variable('get_all')
 # key is per-stream permanent storage
 count = ip_program.key('count')
+state = ip_program.key('state')
 # state constants
-START, MORE, DONE = 0, 1, 2
+START, MORE = 0, 1
+
+finalize = [
+    Assemble(),
+    External([Field('srcip'), Field('dstip')], queue=True),
+    Delete(),
+]
+
+wait_for_more = [
+    Store(state, Constant(MORE))
+]
 
 ip_program.set_code([
     # identifier = hash([packet.srcip, packet.dstip])
@@ -23,56 +33,16 @@ ip_program.set_code([
     # if identifier not in <instance table>:
     #     create instance at <instance table>[identifier]
     #     set <instance table>[identifier].count to 0
-    Perpare(identifier, {count: Constant(0)}),
-    # insert payload into instance's queue, at offset with length
-    Insert(Field('offset'), Field('length'), Field('payload')),
-    # get_all = <there's no hole in queue> && !packet.more_frag
-    Assign(get_all, Op('and', [NoHole(), Op('not', [Field('more_frag')])])),
-    # the only thing could be matched is state
-    # which should always exist in instance permanent data
-    Match({
-        # case state == :start
-        START: [
-            # if packet.dont_frag:
-            #     state = :done
-            # else:
-            #     state = :more
-            # Notice that we pass common integer (START = 0, MORE = 1, DONE = 2) as 
-            # SetState's argument rather than Constant(...)
-            # it's meaningless to SetState(Constant('hello!'))
-            IfElse(Field('dont_frag'), [
-                SetState(DONE),
-            ], [
-                SetState(MORE),
-            ]),
-        ],
-        MORE: [
-            # if get_all: ...
-            IfElse(Rval(get_all), [
-                SetState(DONE),
-            ], [
-                SetState(MORE),
-            ]),
-        ],
-    }),
-    # perm.count = perm.count + 1
+    Prepare(identifier, {count: Constant(0), state: Constant(START)}),
     Store(count, Op('add', [Load(count), Constant(1)])),
-    Match({
-        MORE: [
-            # user_callback_1(packet.field1, packet.field2, perm.count)
-            # the exact external cause (user_callback_1 here) will be added in the future
-            External([Field('field1'), Field('field2'), Load(count)]),
-        ],
-        DONE: [
-            # assemble payloads in the queue
-            Assemble(),
-            # user_callback_2(packet.field1, <queue>)
-            External([Field('field1')], queue=True),
-            External([Field('field1'), Field('field2'), Load(count)]),
-            # tcp_protocol(packet.srcip, packet.dstip, <queue>)
-            External([Field('srcip'), Field('dstip')], queue=True),
-            # delete instance's permanent data from instance table
-            Delete(),
-        ]
-    })
+    # insert payload into instance's queue, at offset with length
+    InsertMeta(Field('offset'), Field('length')),
+    InsertData(Field('offset'), Field('length'), Field('payload')),
+    IfElse(Op('equal', [Load(state), Constant(START)]), [
+        IfElse(SeqSeen('dont_frag'), finalize, wait_for_more)
+    ], [  # MORE
+        IfElse(Field('more_frag'), wait_for_more, finalize),
+    ])
 ])
+
+print(ip_program)
--- rubikir/__init__.py
+++ rubikir/__init__.py
@@ -9,23 +9,48 @@ class Assign(Stat):
         self.variable = variable
         self.expression = expression
 
+    def __str__(self):
+        return 'ASSIGN ' + self.variable.identifier + ' <- ' + str(self.expression)
+
 
 class IfElse(Stat):
     def __init__(self, condition, true_branch, false_branch):
         self.condition = condition
         self.true_branch = true_branch
         self.false_branch = false_branch
+    
+    def __str__(self):
+        ifelse = 'IF ' + str(self.condition) + '\n'
+        for stat in self.true_branch:
+            ifelse += indent(str(stat)) + '\n'
+        ifelse += 'ELSE\n'
+        for stat in self.false_branch:
+            ifelse += indent(str(stat)) + '\n'
+        ifelse += 'IF END'
+        return ifelse
 
 
 class Match(Stat):
     def __init__(self, branch_map):
         self.branch_map = branch_map
 
+    def __str__(self):
+        match = 'MATCH START\n'
+        for case, branch in self.branch_map.items():
+            match += 'CASE ' + str(case) + ':\n'
+            for stat in branch:
+                match += indent(str(stat)) + '\n'
+        match += 'MATCH END'
+        return match
+
 
 class Var:
     def __init__(self, identifier, program):
         self.identifier = identifier
         self.program = program
+
+    def __str__(self):
+        return self.identifier
 
 
 class Expr:
@@ -36,15 +61,32 @@ class Rval(Expr):
     def __init__(self, variable):
         self.variable = variable
 
+    def __str__(self):
+        return 'RVAL ' + str(self.variable)
+
 
 class Field(Expr):
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return 'FIELD ' + self.name
+
+
+class SeqSeen(Expr):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'SEQSEEN ' + self.name
+
 
 class Load(Expr):
     def __init__(self, key):
         self.key = key
+
+    def __str__(self):
+        return 'LOAD ' + str(self.key)
 
 
 class GetState(Expr):
@@ -56,21 +98,43 @@ class Store(Stat):
         self.key = key
         self.value = value
 
+    def __str__(self):
+        return 'STORE ' + str(self.key) + ' <- ' + str(self.value)
+
 
 class SetState(Stat):
     def __init__(self, state):
         self.state = state
 
+    def __str__(self):
+        return 'SETSTATE ' + str(self.state)
 
-class Insert(Stat):
+
+class InsertMeta(Stat):
+    def __init__(self, offset, length):
+        self.offset = offset
+        self.length = length
+
+    def __str__(self):
+        return 'INSERT @' + str(self.offset) + ', len: ' + str(self.length)
+
+
+class InsertData(Stat):
     def __init__(self, offset, length, payload):
         self.offset = offset
         self.length = length
         self.payload = payload
 
+    def __str__(self):
+        return (
+            'INSERTDATA @ ' + str(self.offset) + ', len: ' + str(self.length) +
+            ', payload: ' + str(self.payload)
+        )
+
 
 class Assemble(Stat):
-    pass
+    def __str__(self):
+        return 'ASSEMBLE'
 
 
 class External(Stat):
@@ -78,22 +142,51 @@ class External(Stat):
         self.dep_list = dep_list
         self.queue = queue
 
+    def __str__(self):
+        ext = 'EXTERNAL ' + str_list(self.dep_list)
+        if self.queue:
+            ext += ' AND QUEUE'
+        return ext
+
 
 class Program:
     def __init__(self, queue_api=True):
         self.stat_list = None
         self.key_list = []
+        self.var_list = []
         self.queue_api = queue_api
 
     def variable(self, name):
-        return Var(name, self)
+        var = Var(name, self)
+        self.var_list.append(var)
+        return var
 
     def key(self, name):
-        self.key_list.append(name)
-        return name
+        key = Key(name, self)
+        self.key_list.append(key)
+        return key
 
     def set_code(self, stat_list):
         self.stat_list = stat_list
+
+    def __str__(self):
+        prog = 'Program\n'
+        prog += 'TEMP ' + str_list(self.var_list) + '\n'
+        prog += 'PERM ' + str_list(self.key_list) + '\n'
+        if self.stat_list is not None:
+            prog += '\n'
+            for stat in self.stat_list:
+                prog += str(stat) + '\n'
+        return prog
+
+
+class Key:
+    def __init__(self, identifier, program):
+        self.identifier = identifier
+        self.program = program
+
+    def __str__(self):
+        return self.identifier
 
 
 class Op(Expr):
@@ -101,21 +194,52 @@ class Op(Expr):
         self.type = type
         self.operand_list = operand_list
 
+    def __str__(self):
+        return (
+            'OP:' + self.type + '(' + 
+            str_list([str(operand) for operand in self.operand_list]) +
+            ')'
+        )
+
 
 class NoHole(Expr):
-    pass
+    def __str__(self):
+        return 'NOHOLE'
 
 
-class Perpare(Stat):
+class Prepare(Stat):
     def __init__(self, identifier, initial_map):
         self.identifier = identifier
         self.initial_map = initial_map
+    
+    def __str__(self):
+        return (
+            'PREPARE @ ' + str(self.identifier) + ': ' + 
+            str_dict(self.initial_map)
+        )
 
 
 class Delete(Stat):
-    pass
+    def __str__(self):
+        return 'DELETE'
 
 
 class Constant(Expr):
     def __init__(self, value):
         self.value = value
+
+    def __str__(self):
+        return 'CONST ' + str(self.value)
+
+
+def str_list(list):
+    return ', '.join([str(item) for item in list])
+
+def str_dict(dict):
+    return ', '.join([str(key) + ' => ' + str(value) for key, value in dict.items()])
+
+def indent(str):
+    result = []
+    for line in str.split('\n'):
+        result.append('  ' + line)
+    return '\n'.join(result)
